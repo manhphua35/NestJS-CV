9e94ebc1b81c57f94617dfaa78be3e17bffe2f7c
Tran Manh Phu <manhphu593@gmail.com>
2024-09-11 15:24:44 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
+
+  @Get()
+  @Render('home')
+  handleHomePage() {
+    console.log("Check ", this.configService.get<string>("PORT"))
+    const message1 = this.appService.getHello();
+
+    return {
+      message: message1,
+    };
+    // return "this.appService.getHello()";
+  }
+}
